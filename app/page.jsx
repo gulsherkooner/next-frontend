4bc834d0289dcc2bf468ab8f52ac1fdd7b37a887
gulsherkooner <gulsher.kooner@gmail.com>
2025-05-21 17:02:21 +0530

    .

--- app/page.jsx
+++ app/page.jsx
@@ -26,8 +26,8 @@ export default function Home() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchPublicPosts());
     dispatch(fetchUserData());
+    dispatch(fetchPublicPosts());
   }, [dispatch]);
 
   useEffect(() => {
