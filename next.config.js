215675289386c702622d950699df5e96da66a7e2
gulsherkooner <gulsher.kooner@gmail.com>
2025-05-30 20:42:10 +0530

    .

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      // Only allow known Next.js routes; all others return 404
-      // (nginx will handle API routes)
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-nextjs-route',
-            value: 'true',
-          },
-        ],
-        destination: '/:path*',
-      },
-      {
-        source: '/:path*',
-        destination: '/404',
-      },
-    ];
-  },
+  // reactStrictMode: true,
+  // async rewrites() {
+  //   return [
+  //     // Only allow known Next.js routes; all others return 404
+  //     // (nginx will handle API routes)
+  //     {
+  //       source: '/:path*',
+  //       has: [
+  //         {
+  //           type: 'header',
+  //           key: 'x-nextjs-route',
+  //           value: 'true',
+  //         },
+  //       ],
+  //       destination: '/:path*',
+  //     },
+  //     {
+  //       source: '/:path*',
+  //       destination: '/404',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
